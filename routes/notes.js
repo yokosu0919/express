babde4f7cf2788d5cf4490ca3f29f4f54644fed6
Sanae Yokosu <yokosu@nevision.co.jp>
2024-08-05 15:10:57 +0900

    add env

--- routes/notes.js
+++ routes/notes.js
@@ -1,19 +1,28 @@
 var express = require('express');
+const cors = require('cors'); // corsミドルウェアを追加
 var router = express.Router();
+require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri= "mongodb+srv://yokosu:yokosu@cluster0.qhrto0a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
+//const uri= "mongodb+srv://yokosu:yokosu@cluster0.qhrto0a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const client = new MongoClient(uri);
 
+// corsミドルウェアを使用
+router.use(cors());
+
 router.get('/', async (req, res) => {
-    // データベース、コレクションを指定
-    const database = client.db('notes');
-    const notes = database.collection('notes');
-    // idが１のドキュメントを取得
-    const query = { id: 2 };
-    const note = await notes.findOne(query);
-    res.json(note);
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+
+// idが１のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
+
+res.json(note);
 });
 
 module.exports = router;
